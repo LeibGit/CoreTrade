05f5dd53d4b90f273f7270ab2f354b243ef65fe2
Leib <leibnroth@gmail.com>
2026-02-23 23:07:43 -0500

    update

--- client/src/pages/chart.jsx
+++ client/src/pages/chart.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import { createChart, CandlestickSeries } from "lightweight-charts";
-import "../styles/chart.css"
-
+import "../styles/chart.css";
 
 export default function Chart() {
   const [data, setData] = useState(null);
@@ -12,35 +11,41 @@ export default function Chart() {
     setError(null);
     setLoading(true);
     try {
-      const response = await fetch("https://coretrade.onrender.com/get_current_price");
+      const response = await fetch(
+        "https://coretrade.onrender.com/get_current_price"
+      );
+
       if (!response.ok) {
-      console.log("an error occured fetching data")
-    }
-    const json_res = await response.json();
-    setData(json_res)
+        console.log("an error occured fetching data");
+      }
+
+      const json_res = await response.json();
+      setData(json_res);
     } catch (err) {
-      console.log("an error occured")
-      setError("An error occured, CHECK CHROME TOOLS TO INSPECT")
+      console.log("an error occured");
+      setError("An error occured, CHECK CHROME TOOLS TO INSPECT");
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   if (loading && !data) {
-    return <p>Loading message for wife....</p>
+    return <p>Loading message for wife....</p>;
   }
 
   if (error) {
-    return <p>{error}</p>
+    return <p>{error}</p>;
   }
-  if (!loading && !error) {
-    return (
-      <div className="styling">
-        <button onClick={fetchData}>View Message</button>
-        {data && (
-          <p>{JSON.stringify(data)}</p>
-        )}
-      </div>
+
+  return (
+    <div className="styling">
+      {!data && (
+        <button onClick={fetchData}>
+          View Message
+        </button>
+      )}
+
+      {data && <p>{JSON.stringify(data)}</p>}
+    </div>
   );
-  }
 }
