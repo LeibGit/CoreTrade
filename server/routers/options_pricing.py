71eeb488e8a840b40daf6df04b3f67ffb173b0af
Leib <leibnroth@gmail.com>
2026-02-27 22:59:48 -0500

    fetch stocks

--- server/routers/options_pricing.py
+++ server/routers/options_pricing.py
@@ -7,7 +7,7 @@ router = APIRouter()
 ALPACA_KEY = os.environ.get("ALPACA_KEY")
 ALPACA_SECRET = os.environ.get("ALPACA_SECRET")
 
-url = "https://data.alpaca.markets/v1beta1/options/quotes/latest?symbols=AAPL240119C00150000&feed=opra"
+url = "https://data.alpaca.markets/v2/stocks/{symbol}/quotes/latest"
 
 headers = {
     "accept": "application/json",
@@ -18,4 +18,4 @@ headers = {
 @router.get("/get_current_price")
 async def get_current_price():
     response = requests.get(url, headers=headers)
-    return response.json()
+    return response.json
